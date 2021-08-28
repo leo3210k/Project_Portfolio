a4979534f7831caa47c9109a49011e56be56a41d
Leonardo <leocoelho.pi@gmail.com>
2021-08-27 22:52:29 -0300

    start the project

---
+++ project_portfolio/src/components/template/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default props =>
+    <div class="header">
+      Leonardo Coêlho
+    </div>
---
+++ project_portfolio/src/main/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './App.css'
+import Header from '../components/template/Header'
+
+export default props =>
+    <div class="app">
+      <div className="container">
+        <Header />
+      </div>
+    </div>
